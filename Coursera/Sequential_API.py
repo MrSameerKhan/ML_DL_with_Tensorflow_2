cceec961ba04f64377d97db21c2f415bdcb16672
Sameer Khan <sameerkhanofficial9@gmail.com>
2020-11-24 10:20:22 +0300

    Sequential Course Completed

--- Coursera/Sequential_API.py
+++ Coursera/Sequential_API.py
@@ -769,3 +769,418 @@ model = Sequential([
 model.load_weights("keras_model.hs")
 
 
+
+# Save model weights manually
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping
+
+model = Sequential([
+    Dense(64, activation="sigmoid", input_shape=(10,)),
+    Dense(1)
+])
+
+model.compile(optimizer="sgd", loss="mse", metrics=["mae"])
+early_stoppingg = EarlyStopping(monitor="val_mae", patience=3)
+model.fit(X_train, y_train, validation_split=0.2, epochs=10)
+
+model.save_weights("my_model")
+
+# %%
+# Tutorial Saving and loading model weights
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
+
+(x_train, y_train),(x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+x_train = x_train/255.0
+y_train = y_train/255.0
+
+x_train = x_train[:10000]
+y_train = y_train[:10000]
+x_test = x_test[:1000]
+y_test = y_test[:1000]
+
+fig, ax =  plt.subplots(1, 10, figsize=(10,1))
+for i in range(10):
+    ax[i].set_axis_off()
+    ax[i].imshow(x_train[i])
+
+
+def get_test_accuracy(model, x_test, y_test):
+    test_loss, test_acc = model.evaluate(x=x_test, y=y_test, verbose=1)
+    print("accuracy: {acc:0.3f}".format(acc=test_acc))
+
+
+
+def get_new_model():
+    model = Sequential([
+
+        Conv2D(filters=16, input_shape=(32,32,3), kernel_size=(3,3), activation="relu", name = "Conv_1"),
+        Conv2D(filters=9, kernel_size=(3,3), activation="relu", name = "Conv_2"),
+        MaxPool2D(pool_size=(4,4), name="Pool_1"),
+        Flatten(name = "Flatten"),
+        Dense(units=32, activation="relu", name="dense_1"),
+        Dense(units=10, activation="softmax", name= "dense_2")
+    ])
+
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    return model
+
+model = get_new_model()
+print(model.summary())
+
+checkpoint_path = "Coursera/Sequential_model_checkpoints/checkpoint"
+checkpoint = ModelCheckpoint(filepath=checkpoint_path, frequency="epoch", save_weights_only=True, verbose=1)
+
+model.fit(x=x_train, y=y_train, epochs=3, callbacks=[checkpoint])
+
+
+model = get_new_model()
+get_test_accuracy(model, x_test, y_test)
+
+model.load_weights(checkpoint_path)
+val_acc = get_test_accuracy(model, x_test, y_test)
+
+print(val_acc)
+
+
+
+
+# %%
+
+# Model Saving Criteria
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+model = Sequential([
+
+    Dense(16, activation="relu"),
+    Dropout(0.3),
+    Dense(3, activation="softmax")
+])
+
+model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", 
+            metrics=["acc", "mae"])
+    
+checkpoint = ModelCheckpoint("training_run_1/my_model", save_weights_only=True,
+                            save_best_only=True, monitor="val_acc", mode="max")
+
+
+checkpoint = ModelCheckpoint("training_run_1/my_model.{epoch}.{batch}", 
+        save_weights_only=True, save_freq=1000)
+
+checkpoint = ModelCheckpoint("training_run_1/my_model.{epoch}-{val_loss:.4f}", 
+        save_weights_only=True)
+
+
+model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=10,
+        batch_size=16, callbacks=[checkpoint])
+
+# %%
+
+# Tutorial Model Saving Criteria
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
+
+(x_train, y_train),(x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+x_train = x_train/255.0
+y_train = y_train/255.0
+
+x_train = x_train[:10000]
+y_train = y_train[:10000]
+x_test = x_test[:1000]
+y_test = y_test[:1000]
+
+fig, ax =  plt.subplots(1, 10, figsize=(10,1))
+for i in range(10):
+    ax[i].set_axis_off()
+    ax[i].imshow(x_train[i])
+
+
+def get_test_accuracy(model, x_test, y_test):
+    test_loss, test_acc = model.evaluate(x=x_test, y=y_test, verbose=1)
+    print("accuracy: {acc:0.3f}".format(acc=test_acc))
+
+
+
+def get_new_model():
+    model = Sequential([
+
+        Conv2D(filters=16, input_shape=(32,32,3), kernel_size=(3,3), activation="relu", name = "Conv_1"),
+        Conv2D(filters=9, kernel_size=(3,3), activation="relu", name = "Conv_2"),
+        MaxPool2D(pool_size=(4,4), name="Pool_1"),
+        Flatten(name = "Flatten"),
+        Dense(units=32, activation="relu", name="dense_1"),
+        Dense(units=10, activation="softmax", name= "dense_2")
+    ])
+
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    return model
+
+checkpoint_5000_path = "model_checkpoints_5000/checkpoint_{epoch:02d}_{batch:04d}"
+checkpoint_5000 = ModelCheckpoint(filepath=checkpoint_5000_path,
+                        save_weights_only=True,
+                        save_freq=5000,
+                        verbose=1)
+
+
+model = get_new_model()
+model.fit(x=x_train, y=y_train, epochs=3, validation_data=(x_test, y_test),
+            batch_size=10, callbacks=[checkpoint_5000])
+
+
+
+x_train = x_train[:100]
+y_train = y_train[:100]
+x_test = x_test[:100]
+y_test = y_test[:100]
+
+
+model = get_new_model()
+
+checkpoint_best_path = "model_checkpoints_best/checkpoint"
+checkpoint_best = ModelCheckpoint(filepath=checkpoint_best_path,
+        save_weights_only=True,
+            save_freq="epoch",
+            monitor="val_accuracy",
+            save_best_only=True,
+            verbose=1)
+
+history = model.fit(x=x_train, y=y_train, epochs=50, validation_data=(x_test, y_test),
+        batch_size=10, callbacks=[checkpoint_best],
+            verbose=0)
+
+import pandas as pd
+
+df = pd.DataFrame(history.history)
+df.plot(y=["accuracy", "val_accuracy"])
+
+
+new_model = get_new_model()
+new_model.load_weights(checkpoint_best_path)
+get_test_accuracy(new_model, x_test, y_test)
+
+
+# %%
+
+# Saving entire model
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+model = Sequential([
+    Dense(16, activation="relu"),
+    Dropout(0.3),
+    Dense(3, activation="softmax"),
+])
+
+model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy",
+        metrics=["acc", "mae"])
+
+checkpoint = ModelCheckpoint("my_model", save_weights_only=False)
+# my_model/assets/
+# my_model/saved_model.pb
+# my_model/variables/variables.data-0000-of-0001
+# my_model/variables/variable.index
+
+checkpoint = ModelCheckpoint("keras_model.h5", save_weights_only=False)
+# keras_model.h5
+
+model.save("my_model")
+# Manually save entire model # SavedModel format
+
+model.save("keras_model.h5")
+# HDF5 format keras
+
+model.fit(x_train,y_train, epochs=10, callbacks=[checkpoint])
+
+
+
+
+from tensorflow.keras.model import load_model
+
+# Load tensorflow model
+new_model = load_model("my_model")
+
+# Load keras model
+new_keras_model = load_model("keras_model.h5")
+
+new_model.summary()
+new_model.fit(x_train, y_train, validation_data(X_val, y_val),
+            epochs=20, batch_size=16)
+new_model.evaluate(x_test, y_test)
+new_model.predict(x_sample)
+
+
+# %%
+
+
+# Tutorial Saving the entire model
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
+
+(x_train, y_train),(x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+x_train = x_train/255.0
+y_train = y_train/255.0
+
+x_train = x_train[:10000]
+y_train = y_train[:10000]
+x_test = x_test[:1000]
+y_test = y_test[:1000]
+
+fig, ax =  plt.subplots(1, 10, figsize=(10,1))
+for i in range(10):
+    ax[i].set_axis_off()
+    ax[i].imshow(x_train[i])
+
+
+def get_test_accuracy(model, x_test, y_test):
+    test_loss, test_acc = model.evaluate(x=x_test, y=y_test, verbose=1)
+    print("accuracy: {acc:0.3f}".format(acc=test_acc))
+
+
+
+def get_new_model():
+    model = Sequential([
+
+        Conv2D(filters=16, input_shape=(32,32,3), kernel_size=(3,3), activation="relu", name = "Conv_1"),
+        Conv2D(filters=9, kernel_size=(3,3), activation="relu", name = "Conv_2"),
+        MaxPool2D(pool_size=(4,4), name="Pool_1"),
+        Flatten(name = "Flatten"),
+        Dense(units=32, activation="relu", name="dense_1"),
+        Dense(units=10, activation="softmax", name= "dense_2")
+    ])
+
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    return model
+
+
+checkpoint_path = "model_checkpoints"
+checkpoint = ModelCheckpoint(filepath=checkpoint_path,
+            save_weights_only=False,
+                frequency="epoch",
+                    verbose=1)
+        
+model = get_new_model()
+model.fit(x=x_train, y=y_train, epochs=3,
+            callbacks=[checkpoint])
+            
+print(get_test_accuracy(model, x_test, y_test))
+
+from tensorflow.keras.models import load_model
+
+model = load_model(checkpoint_path)
+print(get_test_accuracy(model, x_test, y_test))
+
+
+# Use the .h5 format to save model
+
+model.save("my_model.h5")
+
+
+model = load_model("my_model.h5")
+print(get_test_accuracy(model, x_test, y_test))
+
+# %%
+
+# Loading pre-trained keras models
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+model = ResNet50(weights="imagenet", include_top=True)
+
+img_input = image.load_img("my_picture.jpg", target_size=(224,224))
+img_input = image.img_to_array(img_input)
+img_input = preprocess_input(img_input[np.newaxis, ...])
+
+preds = model.predict(img_input)
+decode_predictions = decode_predictions(preds, top=3)[0]
+
+
+
+
+# %%
+
+# Tutorial pre-trained keras models
+
+import tensorflow as tf
+from tensorflow.keras.applications import ResNet50
+model = ResNet50(weights="imagenet")
+
+print(model.summary())
+
+
+from tensorflow.keras.preprocessing.image import load_img
+
+lemon_img = load_img("data/lemon.jpg", target_size=(224, 224))
+viaduct_img = load_img("data/viaduct.jpg", target_size=(224, 224))
+water_img = load_img("data/water_tower.jpg", target_size=(224, 224))
+
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.resnet50 import preprocess_input
+import numpy as np
+import pandas as pd
+
+def get_top_5_predictions(img):
+    x = img_to_array(img)[np.newaxis,...]
+    x = preprocess_input(x)
+    preds = decode_predictions(model.predict(x), top=5)
+    top_preds = pd.DataFrame(columns=["prediction", "probability"],
+                    index= np.arrange(5)+1)
+
+
+    for i in range(5):
+        top_preds.loc[i+1, "prediction"] = preds[0][i][1]
+        top_preds.loc[i+1, "probability"] = preds[0][i][2]
+
+    return top_preds
+
+
+get_top_5_predictions(water_img)
+
+
+# %%
