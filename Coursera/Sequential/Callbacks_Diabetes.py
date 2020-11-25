7c0d9123f8155570127abdca79ab67df82fa1c88
Sameer Khan <sameerkhanofficial9@gmail.com>
2020-11-25 13:52:56 +0300

    Nov 25

--- Coursera/Sequential/Callbacks_Diabetes.py
+++ Coursera/Sequential/Callbacks_Diabetes.py
@@ -1,3 +1,5 @@
+# %%
+
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dropout, Dense
@@ -6,16 +8,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
 from tensorflow.keras.callbacks import Callback
 
+# %%
+
 diabetes_dataset = load_diabetes()
+
+# %%
 
 data = diabetes_dataset["data"]
 targets = diabetes_dataset["target"]
 
+# %%
 targets = (targets - targets.mean(axis=0)) / targets.std()
+
+
+# %%
 
 train_data, test_data, train_targets, test_targets = train_test_split(data, targets, test_size=0.1)
 
+# %%
 
+print(train_data.shape)
 def get_regularized_model(wd, rate):
     model=Sequential([
 
@@ -37,6 +49,7 @@ def get_regularized_model(wd, rate):
 
     return model
 
+# %%
 
 class TrainingCallback(Callback):
 
@@ -88,13 +101,23 @@ class PredictionCallback(Callback):
     def on_predicting_end(self, logs=None):
         print("Finished Prediction")
 
+# %%
+
 model = get_regularized_model(1e-5, 0.3)
+print(model.summary())
+
+# %%
 
 model.compile(optimizer="adam", loss="mse")
-
 model.fit(train_data, train_targets, epochs=3, batch_size=128, verbose=False, callbacks=[TrainingCallback()])
+
+# %%
 
 model.evaluate(test_data, test_targets, verbose=False, callbacks=[TestingCallback()])
 
+# %%
+
 model.predict(test_data, verbose=False, callbacks=[PredictionCallback()])
 
+
+# %%
